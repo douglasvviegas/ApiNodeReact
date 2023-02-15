ea0d2626883326aeaba0aef9feaacdd0ad92b064
douglasvviegas <douglasvviegas@gmail.com>
2023-02-14 23:42:57 -0300

    Final BeckEnd

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConfig = 'mongodb+srv://douglas:naovouesquecer420@cluster0.9q2xtxn.mongodb.net/?retryWrites=true&w=majority';
+const dbConfig = 'mongodb+srv://douglas:naovouesquecer420@cluster0.9q2xtxn.mongodb.net/annotations?retryWrites=true&w=majority';
 
 const connection = mongoose.connect(dbConfig, {
     useNewUrlParser: true,
--- backend/src/controllers/annotationConstroller.js
+++ backend/src/controllers/annotationConstroller.js
@@ -1,12 +1,31 @@
-const annotations = require('../models/annotationData')
+const annotations = require('../models/annotationData');
 
 module.exports = {
+    async read(request,response){
+        const annotationList = await annotations.find();
 
-    create(request, response){
-        const{title, notes, priority} = request.body;
-
-        console.log(title);
-        console.log(notes);
-        console.log(priority);
+        return response.json(annotationList);
+    },
+    async create(request, response){
+        const {title, notes, priority} = request.body;
+        if (!notes || !title) {
+            return response.status(400).json({error: "Necessário um título/anotação!"});
+        }
+        const annotationCreated = await annotations.create({
+            title,
+            notes,
+            priority
+        });
+        
+        return response.json(annotationCreated);
+    },
+    async delete(request,response){
+        const {id} = request.params;
+        const annotationDeleted = await annotations.findOneAndDelete({ _id : id });
+        if (annotationDeleted) {
+            return response.json(annotationDeleted);
+        }
+        
+        return response.status(401).json({ error: "Não foi encontrado o registro para deletar!"})
     }
 }
